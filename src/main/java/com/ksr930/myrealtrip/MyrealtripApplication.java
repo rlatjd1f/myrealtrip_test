695b5499843a8889d73a9e8e8cf9911c2e04a1da
rlatj <dl290382@gmail.com>
2026-01-10 08:48:53 +0900

    initial commit

---
+++ src/main/java/com/ksr930/myrealtrip/MyrealtripApplication.java
@@ -0,0 +1,13 @@
+package com.ksr930.myrealtrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyrealtripApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyrealtripApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ksr930/myrealtrip/MyrealtripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ksr930.myrealtrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyrealtripApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
