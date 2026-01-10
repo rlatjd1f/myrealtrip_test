5c46707512be5ba78ed4d6b03ced7f55477cd3fe
rlatj <dl290382@gmail.com>
2026-01-10 15:27:08 +0900

    fix: 에러 코드 상수 값 정리

--- src/main/java/com/ksr930/myrealtrip/common/exception/ErrorCode.java
+++ src/main/java/com/ksr930/myrealtrip/common/exception/ErrorCode.java
@@ -7,7 +7,7 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     INVALID_REQUEST(HttpStatus.BAD_REQUEST, "INVALID_REQUEST", "요청이 올바르지 않습니다."),
     NOT_FOUND(HttpStatus.NOT_FOUND, "NOT_FOUND", "대상을 찾을 수 없습니다."),
-    CONFLICT(HttpStatus.CONFLICT, "요청이 충돌했습니다.");
+    CONFLICT(HttpStatus.CONFLICT, "CONFLICT", "요청이 충돌했습니다.");
 
     private final HttpStatus httpStatus;
     private final String code;
